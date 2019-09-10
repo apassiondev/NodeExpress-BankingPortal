9a216508cb7f25dabe7674bcd11dca7a4ebfb7c2
Andrea Ho <anhhothai.iamcoder@gmail.com>
2019-09-10 16:21:32 +0700

    Using the Express Router

--- src/app.js
+++ src/app.js
@@ -7,11 +7,17 @@ const app = express();
 
 const { accounts, users, writeJSON } = require('./data');
 
+const accountsRoutes = require('./routes/accounts');
+const servicesRoutes = require('./routes/services');
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/accounts', accountsRoutes);
+app.use('/services', servicesRoutes);
 
 app.get('/', (req, res) => {
     res.render('index', {
@@ -20,49 +26,12 @@ app.get('/', (req, res) => {
     })
 });
 
-app.get('/savings', (req, res) => {
-    res.render('account', {
-        account: accounts.savings
-    });
-});
-app.get('/checking', (req, res) => {
-    res.render('account', {
-        account: accounts.checking
-    });
-});
-app.get('/credit', (req, res) => {
-    res.render('account', {
-        account: accounts.credit
-    });
-});
-
 app.get('/profile', (req, res) => {
     res.render('profile', {
         user: users[0]
     });
 });
 
-app.get('/transfer', (req, res) => { res.render('transfer'); });
-app.post('/transfer', (req, res) => {
-    accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
-    accounts[req.body.to].balance = accounts[req.body.to].balance + parseInt(req.body.amount, 10);
 
-    writeJSON();
-
-    res.render('transfer', {message: 'Transfer Completed'});
-});
-
-app.get('/payment', (req, res) => {
-    res.render('payment', {account: accounts.credit});
-});
-app.post('/payment', (req, res) => {
-    accounts.credit.balance -= req.body.amount;
-    accounts.credit.available += parseInt(req.body.amount, 10);
-    writeJSON();
-    res.render('payment', {
-        message: 'Payment successful',
-        account: accounts.credit
-    });
-});
 
 var server = app.listen(3000, () => console.log(`PS Project Running on port ${server.address().port}`));
